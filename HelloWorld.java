97246c5e1dbd70032c7e615707b3a556f4ada66b
Nikcsis211 <ntartin@student.glendale.edu>
2025-01-24 15:43:25 -0800

    Placed the java project inside of the folder

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/* 
+Hello World output
+In java
+*/
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+        
+    }
+}
